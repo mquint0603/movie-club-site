22a1829463b47ce24e29d818f457c1cc34a25d20
Mary Quint <mary.k.quint@gmail.com>
2023-02-15 10:02:48 -0600

    edit fields

--- client/src/constants/movieFields.js
+++ client/src/constants/movieFields.js
@@ -3,7 +3,7 @@ const movieFields = [
   { name: "picker", display: "Who Picked" },
   { name: "year", display: "Year" },
   { name: "genre", display: "Genre" },
-  { name: "director", display: "Animated" },
+  { name: "director", display: "Director" },
   { name: "rating", display: "Rating" },
 ];
 export default movieFields;
